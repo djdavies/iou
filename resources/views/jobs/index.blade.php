c7de0a7bd3c2e5b9f9162717491036a85fd3639c
Jake Davies <jdavies.thfc@gmail.com>
2016-09-12 16:49:50 +0100

    job table

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -1,16 +1,29 @@
 @extends('layouts.master')
 @section('content')
+    <div class="well well-lg">
+        <h2>Here are all the jobs you've applied for.</h2>
+    </div>
     @foreach ($jobs as $job)
         <div class="well well-lg">
-        Job id: {{ $job->id }}
-        <br>
-        Job post_id: {{ $job->post_id }}
-        <br>
-        User id: {{ $job->user_id }}
-        <br>
-        Accepted: {{ $job->accepted }}
-        <br>
-        Test: {{ $job->post->content }}
+            <table class="table">
+                <thead>
+                <tr>
+                    <th>Job Owner</th>
+                    <th>Job Description</th>
+                    <th>Job Accepted By Owner?</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>{{ $job->post->user->name }}</td>
+                    <td>{{ $job->post->content }}</td>
+                    <td>{{ $job->accepted }}</td>
+                </tr>
+                </tbody>
+            </table>
         </div>
     @endforeach
 @endsection
+
+
+
